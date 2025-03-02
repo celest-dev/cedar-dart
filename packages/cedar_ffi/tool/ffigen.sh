c31a02fa0b525ccee8b098a81bf47ac21a12a1eb
Dillon Nys <dillon@celest.dev>
2025-03-02 11:48:36 -0700

    chore: Update dependencies
    - Updates all Dart dependencies
    - Updates cedar_ffi to use latest native assets packages (and removes workaround for Windows)

--- packages/cedar_ffi/tool/ffigen.sh
+++ packages/cedar_ffi/tool/ffigen.sh
@@ -2,6 +2,4 @@
 
 set -e
 
-dart --enable-experiment=native-assets run ffigen --config ffigen.symbols.yaml
-dart --enable-experiment=native-assets run ffigen --config ffigen.loaded.yaml
-dart --enable-experiment=native-assets run ffigen --config ffigen.bundled.yaml
+dart --enable-experiment=native-assets run ffigen --config ffigen.yaml
